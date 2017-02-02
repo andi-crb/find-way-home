9d5c6f356e92bd20a65c2c3dd84f1a16beaee822
Andi <andicbuchanan@gmail.com>
2017-02-02 19:27:01 +0000

    set up

---
+++ index.php
@@ -0,0 +1,82 @@
+<html>
+<head>
+    <title>City</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/3.0.3/normalize.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.css">
+    <link rel="stylesheet" href="/styles/main.css">
+</head>
+<body>
+    <div class-"intro">
+        <p>You wake on a small, lumpy mattress, with your head throbbing and your mouth dry. The sun casts an alien pink light through the small window. You pull your blankets over your head, willing this not to be happening, but an overwhelming nausea overtakes you. You...</p>
+        <p><button>Open the door to try and find a bathroom</button></p>
+        <p><button>Open the window and vomit out of that</button></p>
+        <p><button>Try and settle your stomach by breathing deeply</button></p>
+        <p><span class="planet"></span></p>
+    </div>
+    <div class="character">
+        <form>
+            <label for="name">Your name is:</label>
+            <input class="u-full-width" type="text" placeholder="your name" id="characterName">
+            <label for="exampleRecipientInput">You usually present as:</label>
+            <select class="u-full-width" id="presentation">
+            <option value="Option 1">feminine</option>
+            <option value="Option 2">masculine</option>
+            <option value="Option 3">a mixture of both - it depends on my mood</option>
+            <option value="Option 4">neither masculine or feminine</option>
+            </select>
+            <label for="exampleRecipientInput">Your are:</label>
+            <select class="u-full-width" id="gender">
+            <option value="Option 1">a man</option>
+            <option value="Option 2">a woman</option>
+            <option value="Option 3">sometimes a woman, sometimes a man, sometimes both</option>
+            <option value="Option 4">neither a man nor a woman</option>
+            </select>
+            <label for="exampleRecipientInput">Your most prized possession is:</label>
+            <select class="u-full-width" id="presentation">
+            <option value="Option 1">a man</option>
+            <option value="Option 2">a woman</option>
+            <option value="Option 3">sometimes a woman, sometimes a man, sometimes both</option>
+            <option value="Option 4">neither a man nor a woman</option>
+            </select>
+            <label for="exampleRecipientInput">In your backpack you also carry:</label>
+            <select class="u-full-width" id="presentation">
+            <option value="Option 1">a man</option>
+            <option value="Option 2">a woman</option>
+            <option value="Option 3">sometimes a woman, sometimes a man, sometimes both</option>
+            <option value="Option 4">neither a man nor a woman</option>
+            </select>
+            <label for="exampleRecipientInput">You are most likely to fall in love with:</label>
+            <select class="u-full-width" id="presentation">
+            <option value="Option 1">a man</option>
+            <option value="Option 2">a woman</option>
+            <option value="Option 3">it could be someone of any gender</option>
+            <option value="Option 4">no-one - I love my friends but falling in love isn't a thing that I do</option>
+            </select>
+            <label for="exampleRecipientInput">Your skin is best described as:</label>
+            <select class="u-full-width" id="presentation">
+            <option value="Option 1">dark</option>
+            <option value="Option 2">brown</option>
+            <option value="Option 3">pale</option>
+            <option value="Option 4">green and slightly scale</option>
+            </select>
+            <label for="exampleRecipientInput">Your hair is:</label>
+            <select class="u-full-width" id="presentation">
+            <option value="Option 1">non-existant</option>
+            <option value="Option 2">blond and almost to your waist</option>
+            <option value="Option 3">dark and cropped close to your scalp</option>
+            <option value="Option 4">grey and uncontrollable</option>
+            <option value="Option 5">liable to change colour without warning</option>
+            </select>
+            <label for="exampleRecipientInput">Your preferred weapon is:</label>
+            <select class="u-full-width" id="presentation">
+            <option value="Option 1">non-existant</option>
+            <option value="Option 2">blond and almost to your waist</option>
+            <option value="Option 3">dark and cropped close to your scalp</option>
+            <option value="Option 4">grey and uncontrollable</option>
+            <option value="Option 5">liable to change colour without warning</option>
+            </select>
+        </form>
+    </div>
+    
+</body>
